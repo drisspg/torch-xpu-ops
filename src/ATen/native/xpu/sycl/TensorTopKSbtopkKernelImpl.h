36b302caa46235badc175da111aca921cb16ea5d
Zhang, Jianyi <jianyi.zhang@intel.com>
2026-05-15 12:28:28 +0800

    Add Apache license header to new split compilation files

--- src/ATen/native/xpu/sycl/TensorTopKSbtopkKernelImpl.h
+++ src/ATen/native/xpu/sycl/TensorTopKSbtopkKernelImpl.h
@@ -1,4 +1,14 @@
 /*
+ * Copyright 2020-2026 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+/*
  * Subgroup top-k kernel implementation -- shared header.
  *
  * Contains SubgroupTopKFunctor, sbtopk_launch_impl, and
